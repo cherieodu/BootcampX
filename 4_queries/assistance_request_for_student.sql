5472dfd854d07cb9c67f1be27c73d1520517d0a6
Chérie Oduwole <bellapette0@gmail.com>
2022-01-22 01:54:12 +0000

    Add more queries

---
+++ 4_queries/assistance_request_for_student.sql
@@ -0,0 +1,4 @@
+SELECT count(*) AS total_assistances,  students.name as name FROM assistance_requests
+JOIN students ON assistance_requests.student_id = students.id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
---
+++ 4_queries/cohort_average_assistance_duration.sql
@@ -0,0 +1,7 @@
+SELECT avg(total_duration) as average_total_duration
+FROM (SELECT cohorts.name as name, sum(assistance_requests.completed_at - assistance_requests.started_at) as total_duration
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY total_duration) as avg_sums;
